ccf8c2c68f7c6fc05e4629ad9fc7a83d99fd0a21
Alberto Mateo rey <amateo-r@student.42madrid.com>
2021-09-07 13:10:00 +0200

    norme

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
